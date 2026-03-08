e7826832acb3100088ad91941df0cbe31a716645
michael faith <michaelfaith@users.noreply.github.com>
2026-03-08 16:45:05 -0500

    refactor: simplify `validate` function (#760)
    <!-- 👋 Hi, thanks for sending a PR to package-json-validator! 💖
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [ ] Addresses an existing open issue: fixes #000
    - [ ] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/package-json-validator/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/package-json-validator/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Now that all properties are handled by `validate*` functions, this
    change is prep-work for #752, shifting the conversion from `Result` to
    `string[]` to one place, rather than at each property validation.

--- src/Spec.types.ts
+++ src/Spec.types.ts
@@ -1,9 +1,10 @@
+import type { Result } from "./Result.ts";
+
 export type SpecMap = Record<string, FieldSpec>;
 
 interface FieldSpec {
 	recommended?: boolean;
 	required?: boolean;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	validate: (name: string, obj: any) => string[];
+	validate: (value: unknown) => Result;
 	warning?: boolean;
 }
