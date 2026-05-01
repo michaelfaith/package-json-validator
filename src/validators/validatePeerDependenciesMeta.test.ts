8a0c33844b1d4eb8870d0a2421619ef01f5667ba
michael faith <michaelfaith@users.noreply.github.com>
2026-05-01 03:35:58 -0500

    add test coverage

--- src/validators/validatePeerDependenciesMeta.test.ts
+++ src/validators/validatePeerDependenciesMeta.test.ts
@@ -25,11 +25,19 @@ describe(validatePeerDependenciesMeta, () => {
 		]);
 	});
 
-	it("should return an issue when peerDependenciesMeta is not an object", () => {
+	it("should return an issue when peerDependenciesMeta is not an object (Array)", () => {
 		const result = validatePeerDependenciesMeta(["react"]);
 
 		expect(result.errorMessages).toEqual([
 			"the type should be `object`, not `Array`",
+		]);
+	});
+
+	it("should return an issue when peerDependenciesMeta is not an object (number)", () => {
+		const result = validatePeerDependenciesMeta(13);
+
+		expect(result.errorMessages).toEqual([
+			"the type should be `object`, not `number`",
 		]);
 	});
 
@@ -52,29 +60,48 @@ describe(validatePeerDependenciesMeta, () => {
 		const result = validatePeerDependenciesMeta({
 			"@scope/package": [true],
 			react: null,
+			typescript: 123,
 		});
 
-		expect(result.childResults).toHaveLength(2);
+		expect(result.childResults).toHaveLength(3);
 		expect(result.childResults[0].errorMessages).toEqual([
 			"the value for peer dependency metadata `@scope/package` should be an object, not `Array`",
 		]);
 		expect(result.childResults[1].errorMessages).toEqual([
 			"the value for peer dependency metadata `react` should be an object, not `null`",
 		]);
+		expect(result.childResults[2].errorMessages).toEqual([
+			"the value for peer dependency metadata `typescript` should be an object, not `number`",
+		]);
 	});
 
 	it("should return an issue when optional is not a boolean", () => {
 		const result = validatePeerDependenciesMeta({
+			"@scope/package": {
+				optional: [true],
+			},
 			react: {
 				optional: "true",
 			},
+			typescript: {
+				optional: null,
+			},
 		});
 
-		expect(result.childResults[0].issues).toHaveLength(0);
+		expect(result.childResults).toHaveLength(3);
 		expect(result.childResults[0].childResults).toHaveLength(1);
 		expect(result.childResults[0].childResults[0].issues).toHaveLength(1);
 		expect(result.childResults[0].childResults[0].errorMessages).toEqual([
+			"the `optional` property for peer dependency metadata `@scope/package` should be a boolean, not `Array`",
+		]);
+		expect(result.childResults[1].childResults).toHaveLength(1);
+		expect(result.childResults[1].childResults[0].issues).toHaveLength(1);
+		expect(result.childResults[1].childResults[0].errorMessages).toEqual([
 			"the `optional` property for peer dependency metadata `react` should be a boolean, not `string`",
+		]);
+		expect(result.childResults[2].childResults[0].issues).toHaveLength(1);
+		expect(result.childResults[2].childResults[0].errorMessages).toEqual([
+			"the `optional` property for peer dependency metadata `typescript` should be a boolean, not `null`",
 		]);
 	});
 
