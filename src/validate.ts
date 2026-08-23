76dba3f24a884b91aff95b75e46073c22301dd43
michael faith <michaelfaith@users.noreply.github.com>
2026-08-23 07:47:20 -0500

    feat!: remove legacy `validate` return type (#1036)
    <!-- 👋 Hi, thanks for sending a PR to package-json-validator! 💖
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #755
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/michaelfaith/package-json-validator/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/michaelfaith/package-json-validator/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change completes the rollout of the new return type for `validate`,
    removing the legacy return type and deprecating the second param for
    opting in.
    
    This change also exports a new `ValidateFunction` return type, a
    convenience for packages making heavy use of the validator functions.

--- src/validate.ts
+++ src/validate.ts
@@ -1,5 +1,8 @@
 import { Result } from './Result.ts';
-import type { SpecMap } from './Spec.types.ts';
+import type {
+  PropertyValidations,
+  ValidateFunction,
+} from './validate.types.ts';
 import {
   validateAuthor,
   validateBin,
@@ -33,101 +36,51 @@ import {
 
 // https://docs.npmjs.com/cli/v11/configuring-npm/package-json
 // https://nodejs.org/api/packages.html
-const getSpecMap = (isPrivate: boolean): SpecMap => ({
-  author: {
-    validate: validateAuthor,
-    warning: true,
-  },
-  bin: { validate: validateBin },
-  bugs: {
-    validate: validateBugs,
-    warning: true,
-  },
-  bundledDependencies: {
-    validate: validateBundleDependencies,
-  },
-  bundleDependencies: {
-    validate: validateBundleDependencies,
-  },
-  config: { validate: validateConfig },
-  contributors: {
-    validate: validateContributors,
-  },
-  cpu: { validate: validateCpu },
-  dependencies: {
-    recommended: true,
-    validate: validateDependencies,
-  },
-  description: {
-    validate: validateDescription,
-    warning: true,
-  },
-  devDependencies: {
-    validate: validateDependencies,
-  },
-  directories: {
-    validate: validateDirectories,
-  },
-  engines: {
-    recommended: true,
-    validate: validateEngines,
-  },
-  exports: { validate: validateExports },
-  files: { validate: validateFiles },
-  homepage: {
-    recommended: true,
-    validate: validateHomepage,
-  },
-  keywords: {
-    validate: validateKeywords,
-    warning: true,
-  },
-  license: {
-    validate: validateLicense,
-    warning: true,
-  },
-  main: { validate: validateMain },
-  man: { validate: validateMan },
+const getSpecMap = (isPrivate: boolean): PropertyValidations => ({
+  author: validateAuthor,
+  bin: validateBin,
+  bugs: validateBugs,
+  bundledDependencies: validateBundleDependencies,
+  bundleDependencies: validateBundleDependencies,
+  config: validateConfig,
+  contributors: validateContributors,
+  cpu: validateCpu,
+  dependencies: validateDependencies,
+  description: validateDescription,
+  devDependencies: validateDependencies,
+  directories: validateDirectories,
+  engines: validateEngines,
+  exports: validateExports,
+  files: validateFiles,
+  homepage: validateHomepage,
+  keywords: validateKeywords,
+  license: validateLicense,
+  main: validateMain,
+  man: validateMan,
   name: {
     required: !isPrivate,
     validate: validateName,
   },
-  optionalDependencies: {
-    validate: validateDependencies,
-  },
-  os: { validate: validateOs },
-  peerDependencies: {
-    validate: validateDependencies,
-  },
-  private: { validate: validatePrivate },
-  publishConfig: {
-    validate: validatePublishConfig,
-  },
-  repository: {
-    validate: validateRepository,
-    warning: true,
-  },
-  scripts: { validate: validateScripts },
-  sideEffects: {
-    validate: validateSideEffects,
-  },
-  type: {
-    recommended: true,
-    validate: validateType,
-  },
+  optionalDependencies: validateDependencies,
+  os: validateOs,
+  peerDependencies: validateDependencies,
+  private: validatePrivate,
+  publishConfig: validatePublishConfig,
+  repository: validateRepository,
+  scripts: validateScripts,
+  sideEffects: validateSideEffects,
+  type: validateType,
   version: {
     required: !isPrivate,
     validate: validateVersion,
   },
-  workspaces: {
-    validate: validateWorkspaces,
-  },
+  workspaces: validateWorkspaces,
 });
 
 const parse = (data: string) => {
   if (typeof data != 'string') {
     // It's just a string
-    return 'Invalid data - Not a string';
+    return 'Invalid data: Input should be a string or an object';
   }
   let parsed;
   try {
@@ -136,7 +89,7 @@ const parse = (data: string) => {
   } catch (e: unknown) {
     let errorMessage = 'Invalid JSON';
     if (e instanceof Error) {
-      errorMessage = `Invalid JSON - ${e.toString()}`;
+      errorMessage = `Invalid JSON: ${e.toString()}`;
     }
     return errorMessage;
   }
@@ -146,163 +99,74 @@ const parse = (data: string) => {
     parsed === null ||
     parsed instanceof Array
   ) {
-    return `Invalid JSON - not an object (actual type: ${typeof parsed})`;
+    return `JSON string has invalid type. It should be an object.`;
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return parsed;
 };
 
-export interface ValidationOptions {
-  recommendations?: boolean;
-  warnings?: boolean;
-}
-
-interface LegacyValidationOutput {
-  critical?: Record<string, string> | string;
-  errors?: ValidationError[];
-  recommendations?: string[];
-  valid: boolean;
-  warnings?: string[];
-}
-
-interface ValidationError {
-  field: string;
-  message: string;
-}
-
 /**
  * Validate a package.json object (or string) against the npm spec.
  * @param data The package.json data to validate, either as a string or an object.
- * @param options The options for validation, if using the deprecated spec name parameter.
+ * @param useNewReturnType This parameter is no longer necessary and will be removed in the next major version
  * @returns an object with the validation results.
+ * @deprecated the second parameter is no longer necessary and will be removed in the next major version
  */
-export function validate(
-  data: object | string,
-  options?: ValidationOptions,
-): LegacyValidationOutput;
-
-/**
- * Validate a package.json object (or string) against the npm spec.
- * @param data The package.json data to validate, either as a string or an object.
- * @param useNewReturnType Opt-in to use the new return type (Result) instead of the legacy output.  This is a temporary option to allow users to migrate to the new return type before it becomes the default in a future major release.
- * @returns an object with the validation results.
- */
-export function validate(
-  data: object | string,
-  useNewReturnType: false,
-): LegacyValidationOutput;
-
-/**
- * Validate a package.json object (or string) against the npm spec.
- * @param data The package.json data to validate, either as a string or an object.
- * @param useNewReturnType Opt-in to use the new return type (Result) instead of the legacy output.  This is a temporary option to allow users to migrate to the new return type before it becomes the default in a future major release.
- * @returns an object with the validation results.
- */
+// eslint-disable-next-line @typescript-eslint/unified-signatures
 export function validate(data: object | string, useNewReturnType: true): Result;
+
+/**
+ * Validate a package.json object (or string) against the npm spec.
+ * @param data The package.json data to validate, either as a string or an object.
+ * @returns an object with the validation results.
+ */
+export function validate(data: object | string): Result;
 export function validate(
   data: object | string,
-  optionsOrUseNewReturnType: boolean | ValidationOptions = {},
-): LegacyValidationOutput | Result {
-  // Should we use the new return type?
-  if (typeof optionsOrUseNewReturnType === 'boolean') {
-    if (optionsOrUseNewReturnType) {
-      const result = new Result();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  useNewReturnType?: true,
+): Result {
+  const result = new Result();
 
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      const parsedData = typeof data == 'object' ? data : parse(data);
-
-      // If this is a string, then it's an error message resulting from parsing.
-      if (typeof parsedData == 'string') {
-        throw new Error(parsedData);
-      }
-
-      const map = getSpecMap(
-        (parsedData.private as boolean | undefined) ?? false,
-      );
-
-      const keys = Object.keys(map);
-      for (let i = 0; i < keys.length; i++) {
-        const name = keys[i];
-        const property = map[name];
-
-        if (parsedData[name] === undefined) {
-          if (property.required) {
-            result.addIssue(`Missing required property: ${name}`);
-          }
-          continue;
-        }
-
-        // Each validator returns a Result object that will be a child of the main result.
-        // This allows us to maintain the full structure of the validation results,
-        // including which fields have which issues, and any nested child results for complex fields.
-        const propertyResult = property.validate(parsedData[name]);
-        result.addChildResult(i, propertyResult);
-      }
-
-      return result;
-    } else {
-      return validateLegacy(data, {});
-    }
-  } else {
-    return validateLegacy(data, optionsOrUseNewReturnType);
-  }
-}
-
-const validateLegacy = (
-  data: object | string,
-  options: ValidationOptions,
-): LegacyValidationOutput => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const parsed = typeof data == 'object' ? data : parse(data);
-  const out: LegacyValidationOutput = { valid: false };
+  const parsedData = typeof data == 'object' ? data : parse(data);
 
-  if (typeof parsed == 'string') {
-    out.critical = parsed;
-    return out;
+  // If this is a string, then it's an error message resulting from parsing.
+  if (typeof parsedData == 'string') {
+    throw new Error(parsedData);
   }
 
-  const map = getSpecMap((parsed.private as boolean | undefined) ?? false);
-  const errors: ValidationError[] = [];
-  const warnings: string[] = [];
-  const recommendations: string[] = [];
+  const map = getSpecMap((parsedData.private as boolean | undefined) ?? false);
 
-  let name: keyof typeof map;
-  for (name in map) {
-    const field = map[name];
+  const keys = Object.keys(map);
+  for (let i = 0; i < keys.length; i++) {
+    const name = keys[i];
+    const propertyValidation = map[name];
 
-    if (parsed[name] === undefined) {
-      if (field.required) {
-        errors.push({
-          field: name,
-          message: `Missing required field: ${name}`,
-        });
-      } else if (field.warning) {
-        warnings.push(`Missing recommended field: ${name}`);
-      } else if (field.recommended) {
-        recommendations.push(`Missing optional field: ${name}`);
+    let validate: ValidateFunction;
+    let isRequired = false;
+    if (typeof propertyValidation === 'object') {
+      isRequired = propertyValidation.required;
+      validate = propertyValidation.validate;
+    } else {
+      validate = propertyValidation;
+    }
+
+    // If the property is missing and is required, then add an issue.
+    if (parsedData[name] === undefined) {
+      if (isRequired) {
+        result.addIssue(`Missing required property: ${name}`);
       }
       continue;
     }
 
-    // Validation is expected to return an array of errors (empty means no errors)
-    errors.push(
-      ...field
-        .validate(parsed[name])
-        .errorMessages.map((e) => ({ field: name, message: e })),
-    );
+    // Each validator returns a Result object that will be a child of the main result.
+    // This allows us to maintain the full structure of the validation results,
+    // including which fields have which issues, and any nested child results for complex fields.
+    const propertyResult = validate(parsedData[name]);
+    result.addChildResult(i, propertyResult);
   }
 
-  out.valid = errors.length > 0 ? false : true;
-  if (errors.length > 0) {
-    out.errors = errors;
-  }
-  if (options.warnings !== false && warnings.length > 0) {
-    out.warnings = warnings;
-  }
-  if (options.recommendations !== false && recommendations.length > 0) {
-    out.recommendations = recommendations;
-  }
-
-  return out;
-};
+  return result;
+}
