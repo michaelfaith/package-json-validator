e62645bd2d3b30bd08af404888522a35252924d2
michael faith <michaelfaith@users.noreply.github.com>
2026-09-23 20:16:40 -0500

    ci: enable Are the Types Wrong? on our tsdown build
    This change updates the tsdown config to enable the integration with Are the Types Wrong, for build validation.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig, type UserConfig } from 'tsdown';
 
-const config: UserConfig = defineConfig({});
+const config: UserConfig = defineConfig({
+  attw: {
+    enabled: 'ci-only',
+    level: 'error',
+  },
+});
 
 export default config;
